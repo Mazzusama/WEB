0fa5b4dcfd14800c578ba3e656d5357c882b3f16
Mazzusama <80935037+Mazzusama@users.noreply.github.com>
2022-04-07 15:02:11 +0800

    asd

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <html lang="en">
 <head>
-    <title>Sign Up Form</title>
+    <title>Sign Up Page</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="sign-up-form">
         <img src="./assets/usericon.png">
         <h1>Sign Up Now</h1>
-        <form>
-            <input type="email" class="input-box" placeholder="Your Email">
-            <input type="password" class="input-box" placeholder="Your Password">
+        <form method="post" action="connect.php">
+            <input type="username" name="username" class="input-box" placeholder="Your Username"><br><br>
+            <input type="password" name="password" class="input-box" placeholder="Your Password"><br><br>
             <p><span><input type="checkbox"></span> I agree to the terms of
             services</p>
-            <button type="button" class="signup-btn">Sign up</button>
+            <button type="button" value="Submit" class="signup-btn">Sign up</button>
             <hr>
             <p class="or">OR</p>
             <button type="button" class="FB-btn">Login with Facebook</button>
-            <p>Do you have an account ? <a href="#">Sign in</a></p>
+            <p>Do you have an account ? <a href="signup.php">Sign in</a></p>
             </form>
         </form>
 
---
+++ signup.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up Page</title>
+    <link rel="stylesheet" href="styleforsu.css">
+    <link rel="icon" href="Images/logo3.png"type="image">
+</head>
+<body>
+    
+    <div class="background-image">
+    <h2>Sign Up</h2>
+    <div class="Sign-up">
+    <form method="post" action="signupsql.php">
+    <?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+    <input type="Firstname" class="input-box" placeholder="First name" name="firstname" required>
+    <input type="Lastname" class="input-box" placeholder="Last name" name="lastname" required>
+    <p><input type="username" class="input-box1" placeholder="Username" name="username" oninvalid="('Please Enter Another Username');" oninput="setCustomValidity('')" required></p>
+    <input type="Password" class="input-box1" placeholder="Password"name="password" pattern=".{8,}" oninvalid="this.setCustomValidity('Must contain 8 or more characters');" oninput="setCustomValidity('')" required>
+    <input type="address" class="input-box1" placeholder="Province/Municipality/City/Zip Code" name="address" required>
+    <p><hr>
+    <span>
+    <input type="date" name="bdate" class="form-control" required="" oninvalid="this.setCustomValidity('Please Select Date of Birth');" oninput="setCustomValidity('')"/>
+    </hr></p>
+    <input type="radio" class="gender" name="gender" value="M">Male
+    <input type="radio" class="gender" name="gender" value="F">Female
+
+    <p><span><input type="checkbox" name="c" required="" oninvalid="this.setCustomValidity('Please check this box if you want to proceed');" oninput="setCustomValidity('')"></span> I've read and accept the Terms & Conditions</p>
+    <p><span><input type="checkbox" required="" oninvalid="this.setCustomValidity('Please check this box if you want to proceed');" oninput="setCustomValidity('')"></span > I agree to share my location</p>
+
+
+    <button type="submit" class="signupbutton" value="confirm" name="s">Sign up</button>
+    
+    </form>
+
+</div>
+    </div>
+
+</body>
+
+</html>
